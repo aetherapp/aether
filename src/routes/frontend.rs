db06297060c6918b16a0c0fae05bf9371fea84e8
Dany Sluijk <dany@atlasdev.nl>
2020-05-18 15:26:30 +0200

    refactor: remove the need for .unwrap()

--- src/routes/frontend.rs
+++ src/routes/frontend.rs
@@ -1,22 +1,24 @@
 use include_dir::Dir;
+use rocket::http::Status;
 use std::path::{Path, PathBuf};
 
 static FRONTEND_FILES: Dir = include_dir!("frontend/dist");
 
 #[get("/<file..>")]
-pub fn serve(file: PathBuf) -> &'static [u8] {
+pub fn serve(file: PathBuf) -> Result<&'static [u8], Status> {
 	let path = Path::new("").join(file);
-	let file = FRONTEND_FILES
+
+	FRONTEND_FILES
 		.get_file(path)
 		.or_else(|| FRONTEND_FILES.get_file("index.html"))
-		.unwrap();
-
-	file.contents()
+		.and_then(|file| Some(file.contents()))
+		.ok_or(Status::NotFound)
 }
 
 #[get("/")]
-pub fn serve_index() -> &'static [u8] {
-	let file = FRONTEND_FILES.get_file("index.html").unwrap();
-
-	file.contents()
+pub fn serve_index() -> Result<&'static [u8], Status> {
+	FRONTEND_FILES
+		.get_file("index.html")
+		.and_then(|file| Some(file.contents()))
+		.ok_or(Status::NotFound)
 }
