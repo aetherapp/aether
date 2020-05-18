dcdf35affa1ecd06398be6db26d0d3ac32b604ad
Dany Sluijk <dany@atlasdev.nl>
2020-05-18 17:07:02 +0200

    fix: add yarn to the CI

--- docker-publish.sh
+++ docker-publish.sh
@@ -7,6 +7,11 @@ if [[ -z "${CI}" ]]; then
 	exit 1
 fi
 
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update
+sudo apt install yarn
+
 cd frontend
 yarn install
 cd ..
