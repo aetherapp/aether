ff880c835296f2cda999250f1dd0ec23cc05892e
Dany Sluijk <dany@atlasdev.nl>
2020-05-18 17:17:53 +0200

    feat: add nodejs to deps for CI

--- docker-publish.sh
+++ docker-publish.sh
@@ -10,7 +10,7 @@ fi
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update
-sudo apt install yarn
+sudo apt install yarn nodejs
 
 cd frontend
 yarn install
